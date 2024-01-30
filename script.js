251d30816cbada910bda9ddd4a18867a5a780528
akia-web <59091758+akia-web@users.noreply.github.com>
2024-01-30 14:25:14 +0100

    twitch

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function startVideo() {
 }
 
 function getEmotion(objet){
-  console.log(objet.happy)
   if(objet.happy >= 0.90 && objet.happy <1.1){
      return 'happy'
   }else if(objet.surprised >= 0.90 && objet.surprised <1.1){
@@ -30,27 +29,28 @@ function getEmotion(objet){
 }
 
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, 
     new faceapi.TinyFaceDetectorOptions())
     .withFaceLandmarks()
-    .withFaceExpressions()
+    .withFaceExpressions();
 
-    let emotion = getEmotion(detections[0].expressions)
-    console.log(emotion)
+    const emotion = getEmotion(detections[0].expressions);
 
-    let img = document.getElementById('avatar')
-    img.src =`image/${emotion}.png`
-
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    // faceapi.draw.drawDetections(canvas, resizedDetections)
-    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-
+    let img = document.getElementById('avatar');
+    img.src =`image/${emotion}.png`;
   }, 100)
 })
+
+
+
+setInterval(() =>{
+  const chat = document.getElementsByClassName('chat')
+  chat.innerHtml = 'lala'
+},100)
+
---
+++ test-non-concluent.js
@@ -0,0 +1,89 @@
+
+    // const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    // var context = canvas.getContext("2d");
+    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+
+    // const x1 = resizedDetections[0].landmarks.positions[67].x
+    // const y1 = resizedDetections[0].landmarks.positions[67].y
+
+    // const x2 = resizedDetections[0].landmarks.positions[66].x
+    // const y2 = resizedDetections[0].landmarks.positions[66].y
+
+    // const x3 = resizedDetections[0].landmarks.positions[65].x
+    // const y3 = resizedDetections[0].landmarks.positions[65].y
+
+    // const x4 = resizedDetections[0].landmarks.positions[64].x
+    // const y4 = resizedDetections[0].landmarks.positions[64].y
+
+    // const x5 = resizedDetections[0].landmarks.positions[63].x
+    // const y5 = resizedDetections[0].landmarks.positions[63].y
+
+    // const x6 = resizedDetections[0].landmarks.positions[62].x
+    // const y6 = resizedDetections[0].landmarks.positions[62].y
+
+    // const x7 = resizedDetections[0].landmarks.positions[61].x
+    // const y7 = resizedDetections[0].landmarks.positions[61].y
+
+    // const x8 = resizedDetections[0].landmarks.positions[60].x
+    // const y8 = resizedDetections[0].landmarks.positions[60].y 
+
+    // context.beginPath();
+    // context.moveTo(x1, y1);
+    // context.lineTo(x2, y2);
+    // context.strokeStyle = "yellow"; // Couleur de la ligne
+    // context.lineWidth = 3; // Épaisseur de la ligne
+    // context.stroke();
+
+    // context.beginPath();
+    // context.moveTo(x2, y2);
+    // context.lineTo(x3, y3);
+    // context.strokeStyle = "yellow"; // Couleur de la ligne
+    // context.lineWidth = 3; // Épaisseur de la ligne
+    // context.stroke();
+
+    // context.beginPath();
+    // context.moveTo(x3, y3);
+    // context.lineTo(x4, y4);
+    // context.strokeStyle = "yellow"; // Couleur de la ligne
+    // context.lineWidth = 3; // Épaisseur de la ligne
+    // context.stroke();
+
+    // context.beginPath();
+    // context.moveTo(x4, y4);
+    // context.lineTo(x5, y5);
+    // context.strokeStyle = "yellow"; // Couleur de la ligne
+    // context.lineWidth = 3; // Épaisseur de la ligne
+    // context.stroke();
+
+    // context.beginPath();
+    // context.moveTo(x5, y5);
+    // context.lineTo(x6, y6);
+    // context.strokeStyle = "yellow"; // Couleur de la ligne
+    // context.lineWidth = 3; // Épaisseur de la ligne
+    // context.stroke();
+      
+    // context.beginPath();
+    // context.moveTo(x6, y6);
+    // context.lineTo(x7, y7);
+    // context.strokeStyle = "yellow"; // Couleur de la ligne
+    // context.lineWidth = 3; // Épaisseur de la ligne
+    // context.stroke();
+
+    // context.beginPath();
+    // context.moveTo(x7, y7);
+    // context.lineTo(x8, y8);
+    // context.strokeStyle = "yellow"; // Couleur de la ligne
+    // context.lineWidth = 3; // Épaisseur de la ligne
+    // context.stroke();
+
+    // context.beginPath();
+    // context.moveTo(x8, y8);
+    // context.lineTo(x1, y1);
+    // context.strokeStyle = "yellow"; // Couleur de la ligne
+    // context.lineWidth = 3; // Épaisseur de la ligne
+    // context.stroke();
+
+
+    // faceapi.draw.drawDetections(canvas, resizedDetections)
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
